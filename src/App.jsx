614cb11fee9e8b99121777093404829d6f966203
mhaskesaurabh3 <mhaskesaurabh3@gmail.com>
2023-04-17 12:45:08 +0530

    Search bar UI and card UI added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import RestaurantContainer from './Components/Body/RestaurantContainer';
 import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <RestaurantContainer />
     </div>
   );
 }
